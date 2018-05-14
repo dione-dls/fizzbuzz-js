b76680e776aadf8d485d5c58ae003ee2ea5c6aa7
Salome Lambermont <salome.lambermont@gmail.com>
2018-05-14 17:30:32 +0100

    Complete Fizzbuzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,5 +1,21 @@
 describe("Fizzbuzz", function() {
   it("returns fizz when passed 3", function() {
-    expect(fizzbuzz(3)).toEqual("fizz")
+    expect(fizzbuzz(3)).toEqual("fizz");
+  });
+
+  it("returns buzz when passed 5", function() {
+    expect(fizzbuzz(5)).toEqual("buzz");
+  });
+
+  it("returns fizzbuzz when passed 15", function() {
+    expect(fizzbuzz(15)).toEqual("fizzbuzz");
+  });
+
+  it("returns fizz when passed 6", function() {
+    expect(fizzbuzz(6)).toEqual("fizz");
+  });
+
+  it("returns fizz when passed 2", function() {
+    expect(fizzbuzz(2)).toEqual(2);
   });
 });
